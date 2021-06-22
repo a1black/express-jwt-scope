608d4416eeb6a2d4fa82129816cc26ccf061b548
a1black <37011601+a1black@users.noreply.github.com>
2021-06-22 22:25:18 +0300

    fix: shorten type names in ts type definitions

--- types/express-jwt-scope-tests.ts
+++ types/express-jwt-scope-tests.ts
@@ -3,18 +3,33 @@ import expressJwtScope = require('express-jwt-scope');
 
 const app = express();
 const middleware = expressJwtScope({
-  adminClaimEnabled: true,
+  adminKey: 'admin',
   claimDelimiter: ',',
   claimScopeDelimiter: ':',
   scopeKey: 'scope',
-  scopeRequired: false,
   tokenKey: 'user'
 });
 
 app.use(middleware());
 app.use(middleware('read', 'write'));
-app.use(middleware((scope, helpers) => true, scope => true, () => true));
+app.use(middleware((scope, helpers) => true));
+app.use(middleware(
+  (scope, helpers) => {
+    return !!(
+      helpers.error ||
+      helpers.req ||
+      helpers.claimDelimiter ||
+      helpers.claimScopeDelimiter ||
+      helpers.isAdmin ||
+      helpers.originScope ||
+      helpers.token
+    );
+  })
+);
 app.use(middleware('read').or('write').or('delete'));
 app.use(middleware('read').not('write').not('delete'));
-app.use(middleware(scope => true).or(scope => true).not(scope => true));
+app
+  .use(middleware('read', scope => true)
+  .or('read', scope => true)
+  .not('read', scope => true));
 app.use(middleware('read').promisify());
--- types/index.d.ts
+++ types/index.d.ts
@@ -5,46 +5,63 @@ import express = require('express');
 export = expressJwtScope;
 
 /** Creates a factory function that returns request handling middleware. */
-declare function expressJwtScope(options?: expressJwtScope.Options): expressJwtScope.RequestHandlerFactory;
+declare function expressJwtScope(options?: expressJwtScope.Options): expressJwtScope.Factory;
 
 declare namespace expressJwtScope {
   /** Configuration options. */
   interface Options {
-    adminKey?: string | string[];
+    /** Path to admin claim inside the access token or a callback. */
+    adminKey?: string | string[] | Checker;
+    /** Character separeting permissions if granted permissions described as a string. */
     claimDelimiter?: string;
+    /** Character separating permission's name and scope. */
     claimScopeDelimiter?: string;
+    /** Path to granted permissions inside the access token. */
     scopeKey?: string | string[];
+    /** Path to the access token in HTTP request object. */
     tokenKey?: string | string[];
   }
 
   /** Data available to a custom permission checker. */
-  interface PermissionCheckHelpers {
+  interface Helper {
+    /** Throws ForbiddenError with provided error message. */
     error: (message: string) => never;
+    /** Reference to the HTTP request object. */
     req: express.Request;
+    /** Character separeting permissions if granted permissions described as a string. */
     claimDelimiter: string;
+    /** Character separating permission's name and scope. */
     claimScopeDelimiter: string;
+    /** Whether or not token possesses admin claim. */
     isAdmin?: boolean;
+    /** Copy of granted permissions claim. */
     originScope: string | string[];
+    /** Copy of the access token. */
     token: object;
   }
 
   /** Call signature for custom permission checker. */
-  interface PermissionCheckCallback {
-    (scope?: string[][], helpres?: PermissionCheckHelpers): boolean;
+  interface Checker {
+    (scope: string[][], helpres: Helper): boolean;
   }
-
-  /** Argument type for the middleware factory function. */
-  type RequestedPermission = string | PermissionCheckCallback;
 
   /** Request handling middleware. */
   interface RequestHandler extends express.RequestHandler {
-    not: (permission: RequestedPermission, ...restPermissions: RequestedPermission[]) => RequestHandler;
-    or: (permission: RequestedPermission, ...restPermissions: RequestedPermission[]) => RequestHandler;
+    /** Add negation of requested permission using logical `and` operator. */
+    not: (
+      permission: string | Checker,
+      ...restPermissions: Array<string | Checker>
+    ) => RequestHandler;
+    /** Add alternative set of permissions to check. */
+    or: (
+      permission: string | Checker,
+      ...restPermissions: Array<string | Checker>
+    ) => RequestHandler;
     promisify: () => express.RequestHandler;
   }
 
-  /** Produces request handler that check the access token for requested permissions. */
-  interface RequestHandlerFactory {
-    (...permissions: RequestedPermission[]): RequestHandler;
+  /** Function produces request handler that check the access token for requested permissions. */
+  interface Factory {
+    (...permissions: Array<string | Checker>): RequestHandler;
   }
 }
